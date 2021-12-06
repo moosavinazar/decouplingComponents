9519039e78e144bb9cd616089f84e3e9276c7de5
seyyed hassan moosavi nazar <73283752+moosavinazar@users.noreply.github.com>
2021-12-06 23:35:40 +0330

    before decoupling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {IUser} from "./interfaces/app-interface";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'decouplingComponents';
+  users: IUser[] = [
+    {
+      email: 'hasan@gmail.com',
+      password: '123',
+      address: 'urmia'
+    },
+    {
+      email: 'asiye@gmail.com',
+      password: '123',
+      address: 'khoy'
+    },
+    {
+      email: 'mohamad@gmail.com',
+      password: '123',
+      address: 'paris'
+    },
+
+  ];
+
+  public onAddUser(email: string, password: string, address: string, e: Event) {
+    e.preventDefault();
+    let u: IUser = {
+      email: email,
+      password: password,
+      address: address
+    };
+    this.users.push(u);
+  }
 }
